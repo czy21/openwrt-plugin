c73b2da551b39fbcb4da38736aaee9ce8621d2f9
Bruce Chen <a805899926@gmail.com>
2025-10-01 20:14:57 +0800

    use lede aliyun_ddns.sh

--- main.sh
+++ main.sh
@@ -59,6 +59,12 @@ function sparse_checkout(){
 
 function sparse_checkout_lede(){
 
+  source_lede_dir=feeds/coolsnowwolf/lede
+  source_lede_checkout=
+  source_lede_checkout+="package/lean/ddns-scripts_aliyun/update_aliyun_com.sh"
+  sparse_checkout $source_lede_dir "https://github.com/coolsnowwolf/lede" "$source_lede_checkout"
+  cp -rv $source_lede_dir/package/lean/ddns-scripts_aliyun/update_aliyun_com.sh package/net/ddns-scripts-aliyun/files/
+
   source_luci_dir=feeds/coolsnowwolf/luci
   source_luci_pkg="applications/luci-app-socat applications/luci-app-nfs"
   sparse_checkout $source_luci_dir "https://github.com/coolsnowwolf/luci" "$source_luci_pkg"
@@ -76,9 +82,7 @@ function sparse_checkout_immortalwrt(){
   source_packages_pkg="net/adguardhome"
   source_checkout=
   source_checkout+="$source_packages_pkg"
-  source_checkout+=" net/ddns-scripts/files/usr/lib/ddns/update_aliyun_com.sh"
   sparse_checkout $source_packages_dir "https://github.com/immortalwrt/packages" "$source_checkout" $([ "$branch" == "main" ] && echo master || echo $branch)
-  cp -rv $source_packages_dir/net/ddns-scripts/files/usr/lib/ddns/update_aliyun_com.sh package/net/ddns-scripts-aliyun/files/
 
   for t in $source_packages_pkg;do
     cp_pkg_var $source_packages_dir/$t/Makefile package/$t/Makefile
