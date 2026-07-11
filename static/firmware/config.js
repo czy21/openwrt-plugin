86121f9038a0f3b3d3467abe57bec4f4be84b113
Bruce Chen <805899926@qq.com>
2026-07-11 15:07:23 +0800

    add apk.pub

--- static/firmware/config.js
+++ static/firmware/config.js
@@ -20,7 +20,10 @@ var config = {
   asu_repositories: {
     "openwrt_plugin": "https://openwrt-download.czy21.com/{package_rel}/{arch_packages}/plugin",
   },
-  asu_repository_keys: ["RWSYv7CuMTatNjIKHxlsAiKEvMYwGsJtdFv26IOGFGJZQWa9B5U0Sbc/"],
+  asu_repository_keys: [
+    "RWSYv7CuMTatNjIKHxlsAiKEvMYwGsJtdFv26IOGFGJZQWa9B5U0Sbc/",
+    "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEb+QuIKFsXXV5Dg595V90FnZ8A/h7\ns58gie9uH0DZWt8JtEhsHh7x/u8+ZFjs5gR0RdR36feHvfVi+f1AQ3fPHA==\n-----END PUBLIC KEY-----"
+  ],
   repository_mirrors: [
     "https://downloads.openwrt.org",
     "https://mirrors.ustc.edu.cn/openwrt"
